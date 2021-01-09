eb8ff4a1d354e26b50d2c0bb39abb49c8f34e938
LucianoSm20 <luciano.sanmartin.h@gmail.com>
2021-01-09 18:54:36 -0300

    creacion de diseño de ramos
    se pueden crear diseño de ramos aleatoriamente

--- Elementos_diseno_ramo.py
+++ Elementos_diseno_ramo.py
@@ -1,36 +1,58 @@
 import string 
 import random 
 
-Cantidad_flores_por_ramo = 30
-a = 0
-suma_flores = 0
-lista_letras = ['a', 'b', 'c', 'd', 'e', 'f']
+for i in range(0,4):
+    def eliminar_letra(lista, letra):
 
-Nombre_ramo = random.choice(lista_letras).upper()
-Tamano_ramo=['L', 'S']
-a=random.randint(0,1)
+        for i in lista:
+            if i == letra:
+                lista.remove(letra)
+                break
 
-Tipo_flor_1 = random.choice(lista_letras).lower()
-Cantidad_flor_1 = 0
-for x in range(1): 
-    Cantidad_flor_1 = random.randint(1,10)
+        return lista
 
-Tipo_flor_2 = random.choice(lista_letras).lower()
-Cantidad_flor_2 = 0
-for x in range(1): 
-    Cantidad_flor_2 = random.randint(1,10)
+    Cantidad_flores_por_ramo = 30
+    a = 0
+    suma_flores = 0
+    lista_letras = ['a', 'b', 'c', 'd', 'e', 'f']
 
-Tipo_flor_3 = random.choice(lista_letras).lower()
-Cantidad_flor_3 = 0
-for x in range(1): 
-    Cantidad_flor_3 = random.randint(1,10)
+    Nombre_ramo = random.choice(lista_letras).upper()
+    Tamano_ramo=['L', 'S']
+    a=random.randint(0,1)
 
-suma_flores = (Cantidad_flor_1 + Cantidad_flor_2 + Cantidad_flor_3)
-if suma_flores == Cantidad_flores_por_ramo:
-    print ("Tipo flor: ", str(Nombre_ramo) + str(Tamano_ramo[a]) + str(Tipo_flor_1) + str(Cantidad_flor_1)+ str(Tipo_flor_2) + str(Cantidad_flor_2) + str(Tipo_flor_3) + str(Cantidad_flor_3))
-else:
-    Tipo_flor_complementaria = random.choice('h').lower()
-    Cantidad_flor_complementaria = (Cantidad_flores_por_ramo - suma_flores)
-    print ("Tipo flor: ", str(Nombre_ramo) + str(Tamano_ramo[a]) + str(Tipo_flor_1) + str(Cantidad_flor_1)+ str(Tipo_flor_2) + str(Cantidad_flor_2) + str(Tipo_flor_3) + str(Cantidad_flor_3) + str(Tipo_flor_complementaria) + str(Cantidad_flor_complementaria))
+    Tipo_flor_1 = random.choice(lista_letras).lower()
+    lista_letras = eliminar_letra(lista_letras, Tipo_flor_1)
+    Cantidad_flor_1 = 0
+    for x in range(1): 
+        Cantidad_flor_1 = random.randint(1,10)
+
+    Tipo_flor_2 = random.choice(lista_letras).lower()
+    lista_letras = eliminar_letra(lista_letras, Tipo_flor_2)
+    Cantidad_flor_2 = 0
+    for x in range(1): 
+        Cantidad_flor_2 = random.randint(1,10)
+
+    Tipo_flor_3 = random.choice(lista_letras).lower()
+    lista_letras = eliminar_letra(lista_letras, Tipo_flor_3)
+    Cantidad_flor_3 = 0
+    for x in range(1): 
+        Cantidad_flor_3 = random.randint(1,10)
+
+    suma_flores = (Cantidad_flor_1 + Cantidad_flor_2 + Cantidad_flor_3)
+
+    flores = [Tipo_flor_1, Tipo_flor_2, Tipo_flor_3]
+    lista = sorted(flores)
+    print(lista)
+    #if suma_flores == Cantidad_flores_por_ramo:
+
+    Tipo_flor = str(Nombre_ramo) + str(Tamano_ramo[a]) + str(lista[0]) + str(Cantidad_flor_1)+ str(lista[1]) + str(Cantidad_flor_2) + str(lista[2]) + str(Cantidad_flor_3) + '30'
+
+
+
+    print(Tipo_flor)
+
+    diseño=open("data/diseno_ramos.txt","a") 
+    diseño.write(Tipo_flor + '\n')
+    
 
 
